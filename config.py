bdca486ab36ff49f804cdc9997ecbf2115c48dc8
Swordigo <140787949+swordigo007@users.noreply.github.com>
2026-02-19 20:31:17 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+BASE_URL = os.getenv("BASE_URL")  
+# railway app domain e.g https://telegram-streambot.up.railway.app
